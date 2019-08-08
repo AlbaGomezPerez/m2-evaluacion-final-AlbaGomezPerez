dac630d9bcc8707cb2a38a494e682f9337a9d640
Alba Gómez Pérez <albagope@gmail.com>
2019-08-08 11:41:53 +0200

    update search part

---
+++ _src/assets/js/favourites.js
@@ -0,0 +1,20 @@
+// FAVORITOS
+// const favs=[];
+// function myFavourite(){
+//   const myfav = event.currentTarget;
+//   myfav.classList.toggle ('favourite');
+//   if(myfav.classList.contains('favourite'));
+//   if(favs.includes(myfav)===false){
+//   favs.push(id);
+//   }else{
+//     const favouriteSerie = favs.favouriteSerieOf(id);
+//     if(favouriteSerie > -1){
+//       favs.splice(favouriteSerie, 1);
+//     }
+//   }
+// }
+function myFavourite(){
+  serie.classList.add('favourite');
+}
+
+series.addEventListener('click', myFavourite);
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -17,15 +17,17 @@ function serching(){
 // pintar en el espacio de "series" las imágenes y títulos
     series.innerHTML='';
     for (let i = 0; i < data.length; i++){
-    let serie = '<div class="serie">';
-    serie += '<div class="space-title">' +data[i].show.name+ '</div>';
-    serie += '<div class="space-image"><img src="' + data[i].show.image.medium + '"/></div>';
+    let serie = '<div class="serie-space">';
+    serie += '<ul class="serie" >';
+    serie += '<li class="space-title">' +data[i].show.name+ '</li>';
+    serie += '<li class="space-image"><img src="' + data[i].show.image.medium + '"/></li>';
+    serie += '</ul>';
     serie += '</div>';
     series.innerHTML += serie;
-    console.log(serie);
 
 
-// Si no tiene imagen medium, ponerle foto por defecto.
+    // IMAGEN POR DEFECTO
+    // Si no tiene imagen medium, ponerle foto por defecto.
     // if(data[i].show.image === null){
     //   '<div class="space-image"><img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" /></div>';
     //   }else{
@@ -35,7 +37,7 @@ function serching(){
 });
 }
 
-
+// Al clickar sobre el botón, busca
 button.addEventListener('click', serching);
 // Al pulsar la tecla 13 (enter) también busca. Esto se ejecuta en la función "serching"
 inputValue.addEventListener('keyup', event => {
@@ -43,4 +45,6 @@ inputValue.addEventListener('keyup', event => {
     serching();
   }
   });
+
+
 
