78efc8f66e5f057144d7418f3629c47f03de7e43
Alba Gómez Pérez <albagope@gmail.com>
2019-10-12 16:47:44 +0200

    function new url on info page

--- _src/assets/js/info.js
+++ _src/assets/js/info.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let infoUrl = new URL(window.location.search);
+let id = infoUrl.searchParams.get('id');
+// console.log('soy el ' +(id)+'y funciono');
+
+
 // api de id
 
 // meter variable id de serie
