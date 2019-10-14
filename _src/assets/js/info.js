03ea4aa1121c1a07ec7f2e3716cbf1e19e1127ed
Alba Gómez Pérez <albagope@gmail.com>
2019-10-14 14:52:30 +0200

    Info JavaScript

--- _src/assets/js/info.js
+++ _src/assets/js/info.js
@@ -4,8 +4,42 @@ let infoUrl = new URL(window.location.href);
 let id = infoUrl.searchParams.get('id');
 console.log(id);
 
+const series = document.querySelector('.space-series');
+const urlId = 'http://api.tvmaze.com/shows/';
 
-// api de id http://api.tvmaze.com/shows/
 
-// fetch que me devuelva:
-// -titulo. imagen. summary
+  fetch(urlId + id)
+    .then(response => response.json())
+    .then(data => {
+// pintar en el espacio de "series" las imágenes y títulos
+      series.innerHTML = '';
+// Bucle para recorrer la info devuelta (data). Empiezo en el elemento 0,
+// y añado cada vez uno.
+        // variable que hace referencia al id, el name y la imagen.
+        let serieDetail = {
+          id: data.show.id,
+          name: data.show.name,
+          summary: data.show.summary,
+        };
+        if (data.show.image === null) {
+          serieDetail.imageMedium = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        } else {
+          serieDetail.imageMedium = data.show.image.medium;
+        }
+        series.innerHTML += generateSerieContentDetail(serieDetail);
+      }
+    );
+
+function generateSerieContentDetail(serieDetail) {
+  let serieMoreInfo = '<div class="serie-space">';
+  serieMoreInfo += '<ul class="serie" >';
+  serieMoreInfo += '<li class="space-title ' + serieDetail.name + '</li>';
+  // añadir etiqueta imagen (poner foto por defecto si no tiene la serie)
+  serieMoreInfo += '<li class="space-image"><img src="' + serieDetail.imageMedium + '"/></li>';
+  serieMoreInfo += '<li class="space-summary ' + serieDetail.summary + '"></li>';
+  serieMoreInfo += '</ul>';
+  serieMoreInfo += '</div>';
+  console.log(serieDetail.id);
+  console.log('El id de la serie es ' + (serieDetail.id) + ' y lo voy a usar');
+  return serieMoreInfo;
+}
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -13,50 +13,51 @@ const url = 'http://api.tvmaze.com/search/shows?q=';
 
 //función hace petición a la api con el valor introducido
 // por el usuario
-function serching(){
+function serching() {
   fetch(url + (inputValue.value || inputGenre.value))
-  .then(handleErrors)
-  .then (response => response.json())
-  .then (data => {
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(data => {
 // pintar en el espacio de "series" las imágenes y títulos
-    series.innerHTML='';
+      series.innerHTML = '';
 // Bucle para recorrer la info devuelta (data). Empiezo en el elemento 0,
 // y añado cada vez uno.
-    for (let i = 0; i < data.length; i++){
-  // variable que hace referencia al id, el name y la imagen.
-      let serieInfo = {
-        id: data[i].show.id,
-        name: data[i].show.name,
-      };
-      if(data[i].show.image === null){
-        serieInfo.imageMedium = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-      }else{
-        serieInfo.imageMedium = data[i].show.image.medium;
+      for (let i = 0; i < data.length; i++) {
+        // variable que hace referencia al id, el name y la imagen.
+        let serieInfo = {
+          id: data[i].show.id,
+          name: data[i].show.name,
+        };
+        if (data[i].show.image === null) {
+          serieInfo.imageMedium = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+        } else {
+          serieInfo.imageMedium = data[i].show.image.medium;
+        }
+
+        // Dentro de series (antes estaba vacio) meto la variable, codificada
+        // más adelante necesito esa info codificada.
+        series.innerHTML += generateSerieContent(serieInfo);
       }
 
-      // Dentro de series (antes estaba vacio) meto la variable, codificada
-      // más adelante necesito esa info codificada.
-      series.innerHTML += generateSerieContent(serieInfo);
-    }
 
+      // función ejecutada más adelante. Actulizar info favoritos.
+      updateFavoriteClickEvent();
+    })
 
-    // función ejecutada más adelante. Actulizar info favoritos.
-    updateFavoriteClickEvent();
-  })
-
-  // mensaje para cuando de un error el servidor.
-  .catch(function(error) {
-    console.log(error);
-    series.innerHTML = 'Vuelve a intentarlo más tarde, se ha producido un error';
-  });
+    // mensaje para cuando de un error el servidor.
+    .catch(function (error) {
+      console.log(error);
+      series.innerHTML = 'Vuelve a intentarlo más tarde, se ha producido un error';
+    });
 }
+
 // Al clickar sobre el botón, busca
 searchButton.addEventListener('click', serching);
 searchButton2.addEventListener('click', serching);
 
 // Al pulsar la tecla 13 (enter) también busca. Esto se ejecuta en la función "serching"
 inputValue.addEventListener('keyup', event => {
-  if (event.keyCode === 13){
+  if (event.keyCode === 13) {
     serching();
   }
 });
@@ -66,54 +67,54 @@ inputValue.addEventListener('keyup', event => {
 // -se parsea la info y se guarda en localStorage
 // -cambias de una clase a otra (favoritos).
 // -Dependiendo de la clase, éste se añade o se elimina de favoritos
-function favSav(event){
+function favSav(event) {
   let favouriteSerieList = JSON.parse(localStorage.getItem('favouriteSerieListStored'));
   let listaFavoritosContieneSerie = false;
-  if(favouriteSerieList !== null && favouriteSerieList !== undefined){
+  if (favouriteSerieList !== null && favouriteSerieList !== undefined) {
     listaFavoritosContieneSerie = favouriteSerieList.includes(event.target.id);
   }
 
-  if (!listaFavoritosContieneSerie){
+  if (!listaFavoritosContieneSerie) {
     //agregar favorito
     event.target.classList.add('like');
     event.target.classList.remove('noLike');
 
-    if (favouriteSerieList === null || favouriteSerieList === undefined){
+    if (favouriteSerieList === null || favouriteSerieList === undefined) {
       favouriteSerieList = [event.target.id];
-    }else{
+    } else {
       favouriteSerieList.push(event.target.id);
     }
-  }else{
+  } else {
     // borrar favorito.
-    const seriesToUnfavorite = document.querySelectorAll("[id='" + event.target.id + "']");
-    for(let serieToUnfavorite of seriesToUnfavorite){
+    const seriesToUnfavorite = document.querySelectorAll('[id=\'' + event.target.id + '\']');
+    for (let serieToUnfavorite of seriesToUnfavorite) {
       serieToUnfavorite.classList.add('noLike');
       serieToUnfavorite.classList.remove('like');
     }
     //
-    if (favouriteSerieList !== null || favouriteSerieList !== undefined){
-      favouriteSerieList = favouriteSerieList.filter(function(value, index, arr){
+    if (favouriteSerieList !== null || favouriteSerieList !== undefined) {
+      favouriteSerieList = favouriteSerieList.filter(function (value, index, arr) {
         return value !== event.target.id;
       });
     }
   }
 
   // recoger la info del localStorage. Hay que devolverla a su estado natural, a cadena
-  localStorage.setItem( 'favouriteSerieListStored', JSON.stringify(favouriteSerieList));
+  localStorage.setItem('favouriteSerieListStored', JSON.stringify(favouriteSerieList));
   showFavourites();
 }
 
 // Función id. Los id guardados en el localStorage
 // los recoge de nuevo para volver a pintarlos y les pone la clase correspondiente.
-function isFavouriteSerie(serieInfo){
+function isFavouriteSerie(serieInfo) {
   let favouriteId = localStorage.getItem('favouriteSerieListStored');
-  if (favouriteId === null || favouriteId === undefined){
+  if (favouriteId === null || favouriteId === undefined) {
     return 'noLike';
-  }else{
+  } else {
     favouriteId = JSON.parse(favouriteId);
-    if(favouriteId.includes(serieInfo)) {
+    if (favouriteId.includes(serieInfo)) {
       return 'like';
-    }else{
+    } else {
       return 'noLike';
     }
   }
@@ -128,26 +129,28 @@ function handleErrors(response) {
   }
   return response;
 }
+
 // pintar los favoritos. Esta función es llamada en
 //  "removing" y cuando guardamos por primera vez en local
 // (cuando borremos). Hay que descodificarlos, antes los
 // habíamos codificado. En la parte de favouriteSpace metemos
 // generateSerieContent que es donde aparecen las series buscadas.
-function showFavourites(){
+function showFavourites() {
   favouriteSpace.innerHTML = '';
-  const favouriteSerieList = JSON.parse(localStorage.getItem( 'favouriteSerieListStored'));
-  if(favouriteSerieList != undefined && favouriteSerieList != null){
-    for (let favouriteSerie of favouriteSerieList){
+  const favouriteSerieList = JSON.parse(localStorage.getItem('favouriteSerieListStored'));
+  if (favouriteSerieList != undefined && favouriteSerieList != null) {
+    for (let favouriteSerie of favouriteSerieList) {
       const favouriteSerieInfo = JSON.parse(decodeURIComponent(favouriteSerie));
       favouriteSpace.innerHTML += generateSerieContent(favouriteSerieInfo);
     }
   }
   updateFavoriteClickEvent();
 }
+
 showFavourites();
 
 // función generar estructura para el html
-function generateSerieContent(serieInfo){
+function generateSerieContent(serieInfo) {
   const serieInfoStr = encodeURIComponent(JSON.stringify(serieInfo));
   let serie = '<div class="serie-space">';
   serie += '<ul class="serie" >';
@@ -158,13 +161,13 @@ function generateSerieContent(serieInfo){
   serie += '<li class="info"> <a class="link" href= "info.html?id=' + (serieInfo.id) + '"></a></li>';
   serie += '</ul>';
   serie += '</div>';
-  console.log (serieInfo.id);
+  console.log(serieInfo.id);
   console.log('El id de la serie es ' + (serieInfo.id) + ' y lo voy a usar');
   return serie;
 }
 
 
-function removing(){
+function removing() {
   localStorage.clear();
   showFavourites();
 }
@@ -173,21 +176,21 @@ reset.addEventListener('click', removing);
 
 // recorre toda la info sobre la parte de favoritos. Es llamada dentro de 2
 // funciones "serching"(petición) y "showFavourites"(sección favoritos)
-function updateFavoriteClickEvent(){
-  for(let serieNoLike of document.querySelectorAll('.noLike')){
+function updateFavoriteClickEvent() {
+  for (let serieNoLike of document.querySelectorAll('.noLike')) {
     serieNoLike.addEventListener('click', favSav);
   }
 
-  for(let myFav of document.querySelectorAll('.like')){
+  for (let myFav of document.querySelectorAll('.like')) {
     myFav.addEventListener('click', favSav);
   }
 }
 
 // función colapsables
-function openFavourites(){
-  if(favouritesSaved.classList.contains('hidden')){
+function openFavourites() {
+  if (favouritesSaved.classList.contains('hidden')) {
     favouritesSaved.classList.remove('hidden');
-  }else{
+  } else {
     favouritesSaved.classList.add('hidden');
   }
 }
