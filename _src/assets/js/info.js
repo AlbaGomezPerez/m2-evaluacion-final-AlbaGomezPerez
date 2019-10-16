a0acb5524ac8a257a0e60fa6277b1fdad5812fe9
Alba Gómez Pérez <albagope@gmail.com>
2019-10-16 12:33:53 +0200

    fix fetch on info.js

--- _src/assets/js/info.js
+++ _src/assets/js/info.js
@@ -5,13 +5,10 @@ let id = infoUrl.searchParams.get('id');
 console.log(id);
 
 /*const page = document.querySelector('.page');*/
-const refresh = document.querySelector('.body');
-const series = document.querySelector('.space-series');
-const button = document.querySelector('.here');
+const series = document.querySelector('.page');
 const urlId = 'http://api.tvmaze.com/shows/';
 
 window.onload = function() {
-  function showDetailSerie(){
   fetch(urlId + id)
     .then(response => response.json())
     .then(data => {
@@ -21,36 +18,35 @@ window.onload = function() {
         // y añado cada vez uno.
         // variable que hace referencia al id, el name y la imagen.
         let serieDetail = {
-          id: data.show.id,
-          name: data.show.name,
-          summary: data.show.summary,
+          id: data.id,
+          name: data.name,
+          summary: data.summary,
         };
-        if (data.show.image === null) {
+        if (data.image === null) {
           serieDetail.imageMedium = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
         } else {
-          serieDetail.imageMedium = data.show.image.medium;
+          serieDetail.imageMedium = data.image.medium;
         }
         series.innerHTML += generateSerieContentDetail(serieDetail);
       }
-      );}
-};
+      );
+}
+
 
 function generateSerieContentDetail(serieDetail) {
   let serieMoreInfo = '<div class="serie-space">';
   serieMoreInfo += '<ul class="serie">';
-  serieMoreInfo += '<li class="space-title ' + serieDetail.name + '</li>';
+  serieMoreInfo += '<li class="space-title">' + serieDetail.name + '</li>';
   // añadir etiqueta imagen (poner foto por defecto si no tiene la serie)
   serieMoreInfo += '<li class="space-image"><img src="' + serieDetail.imageMedium + '"/></li>';
-  serieMoreInfo += '<li class="space-summary ' + serieDetail.summary + '"></li>';
+  serieMoreInfo += '<li class="space-summary">' + serieDetail.summary + '</li>';
   serieMoreInfo += '</ul>';
   serieMoreInfo += '</div>';
-  console.log(serieDetail.id);
-  console.log('El id de la serie es ' + (serieDetail.id) + ' y lo voy a usar');
   return serieMoreInfo;
 }
 
 /*page.addEventListener('click',location.reload);*/
-refresh.addEventListener('onload', showDetailSerie);
+// refresh.addEventListener('onload', showDetailSerie);
 
 
 
